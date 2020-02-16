a684a4bd7d4264cf62fa44fdf66e472267c5753f
Yunier Alvarez <yunieralvarez@librem.one>
2020-02-16 11:25:04 -0500

    before video

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import StreetInterception from './components/StreetInterception/StreetInterception';
 
+// TODO: replace project with notebook project
+
 function App() {
 	return (
 		<div>
